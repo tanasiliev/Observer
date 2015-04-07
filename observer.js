fca2829a5a6ef983c50647abd1af6dad3954d31c
Atanas Iliev <tanasiliev@gmail.com>
2015-04-07 13:19:27 +0300

    Update observer.js

--- observer.js
+++ observer.js
@@ -1,54 +1,51 @@
-(function (global){
-
-    var isFunction = function(obj) {
-      return !!(obj && obj.constructor && obj.call);
-    };
-
-    var Signal = function(){
-        this.subscribers = [];
-    };
+(function (global) {
     
-    Signal.prototype = {
-        subscribe : function(handler){
-            if(!isFunction(handler))
-               throw new Error("Invalid Function " + handler); 
-            this.subscribers.push(handler);
-        },
-        publish: function(){
-            var subs = this.subscribers;
-            for(var i = 0; i < subs.length; ++i)
-                subs[i].apply(null, arguments);
-        },
-        detach: function(handler){
-            if(!isFunction(handler))
-                throw new Error("Invalid Function " + handler);
-            var subs = this.subscribers;
-            for(var i = 0; i < subs.length; ++i){    
-                if(subs[i] == handler){ 
-                    subs.splice(i, 1);
-                    break;  
-              }
-           }    
-        },
-        detachAll: function(){
-            this.subscribers = [];
-        },
-        listeners : function(){
-            return this.subscribers.slice(0);
-        }
+   'use strict';
+   
+    var isFunction = function (obj) {
+        return !!(obj && obj.constructor && obj.call);
     };
-    
+
+    var Signal = function () {
+        var _subscribers = [];
+        var signal = {
+            subscribe: function (handler) {
+                if (!isFunction(handler))
+                    throw new Error("Invalid Function " + handler);
+                _subscribers.push(handler);
+            },
+            publish: function () {
+                var subs = _subscribers;
+                for (var i = 0; i < subs.length; ++i)
+                    subs[i].apply(null, arguments);
+            },
+            detach: function (handler) {
+                if (!isFunction(handler))
+                    throw new Error("Invalid Function " + handler);
+                var subs = _subscribers;
+                for (var i = 0; i < subs.length; ++i) {
+                    if (subs[i] == handler) {
+                        subs.splice(i, 1);
+                        break;
+                    }
+                }
+            },
+            detachAll: function () {
+                _subscribers = [];
+            },
+            listeners: function () {
+                return _subscribers;
+            }
+        };
+        return signal;
+    };
+
     var observer = {
-        create : function(){
-            var signal = new Signal(),
-                obj = {};
-            for(var prop in Signal.prototype){
-                obj[prop] = signal[prop].bind(signal);
-            } 
-            return obj;
+        create: function () {
+            return Signal();
         }
     };
-
+    
     //exports to multiple environments
     if(typeof define === 'function' && define.amd){ 
         define(function () { return observer; });
@@ -57,5 +54,5 @@
     } else { 
         global['observer'] = observer;
     }
-    
+
 })(this);
